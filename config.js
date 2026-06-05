33542b93d3c203ac30cf56d5b9c821282d97bbe5
Otto-9092 <124396939+Otto-9092@users.noreply.github.com>
2026-06-05 10:42:55 -0600

    Update config.js

--- config.js
+++ config.js
@@ -9,3 +9,4 @@
 // (e.g. https://YOUR-GITHUB-USERNAME.github.io/*)
 // ============================================================
 window.GOOGLE_MAPS_API_KEY = "AIzaSyCkhKfboH2vsdIF4p1zb6ObrWWD3Z8gqZ8";
+window.GOOGLE_OAUTH_CLIENT_ID = "1097127772986-0r37oebfdo07mhsij1d1hq2oe0o7qf9h.apps.googleusercontent.com";
